3b945a0408056f6633346f687908f458f9d877db
jalexandermetal <jametalrs@gmail.com>
2021-04-14 01:13:02 -0500

    segundo dia de avance

---
+++ menu.php
@@ -0,0 +1,29 @@
+<nav class="navbar  navbar-expand-lg navbar-dark bg-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="index.php">
+    <span class="fas fa-fingerprint fa-2x"></span>
+    </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item active">
+          <a class="nav-link active" aria-current="page" href="index.php">
+         <span class= "fas fa-laptop-house"></span> Inicio <span class="sr-only"(current)></span>
+          </a>
+        </li>
+        <li class="nav-item active">
+          <a class="nav-link" href="contactos.php">
+         <span class="fas fa-users"> </span> Contactos
+          </a>
+        </li>
+        <li class="nav-item active">
+          <a class="nav-link" href="categorias.php" tabindex="-1" aria-disabled="true">
+          <span class="fas fa-cubes"></span> Categorias
+          </a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
