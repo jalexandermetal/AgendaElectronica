9a66dcc6b4b0c5d17db9854ed4233a709371e82d
jalexandermetal <jametalrs@gmail.com>
2021-06-05 22:59:56 -0500

    estamos probrando como es le proceso

--- index.php
+++ index.php
@@ -1,8 +1,10 @@
+<p>Ejercicio de aprendizaje eleborado por Jhoan Rodriguez</p>
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
     <meta charset="UTF-8">
+
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Agenda de contactos</title>
@@ -27,23 +29,20 @@
                     <div class="card">
                         <div class="card-body">
                             <ul class="list-group">
-                                <li class="list-group-item">Name</li>
-                                <li class="list-group-item">Surname</li>
-                                <li class="list-group-item">Date of Birth</li>
-                                <li class="list-group-item">Place of birth</li>
+                                <li class="list-group-item">Nombre</li>
+                                <li class="list-group-item">Apellido </li>
+                                <li class="list-group-item">Fecha de nacimiento</li>
+                                <li class="list-group-item">email</li>
+                                <li class="list-group-item">telefono</li>
                             </ul>
                         </div>
                     </div>
                 </div>
             </div>
             <hr class="my-4">
-            <p>Ejercicio de aprendizaje eleborado por Jhoan Rodriguez</p>
             <p class="lead">
             </p>
         </div>
-
-
-
     </div>
 
 </body>
